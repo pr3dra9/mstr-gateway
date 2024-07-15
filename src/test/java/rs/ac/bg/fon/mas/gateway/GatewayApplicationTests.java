7b772b0b8fe61c93b389bac262b6c5b2cac8f162
Predrag Đokić <predragdj@terieng.co.rs>
2024-07-15 14:12:56 +0200

    Enabled Discovery Client.

--- src/test/java/rs/ac/bg/fon/mas/gateway/GatewayApplicationTests.java
+++ src/test/java/rs/ac/bg/fon/mas/gateway/GatewayApplicationTests.java
@@ -3,7 +3,7 @@ package rs.ac.bg.fon.mas.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"eureka.client.enabled=false"})
 class GatewayApplicationTests {
 
 	@Test
